f424bda6ed34a2eb0760f8b3e2abbfa0117eaf5b
Pavel Lyashenko <zp.redwasp@gmail.com>
2023-01-28 11:13:25 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Position
+
+A description of this package.
---
+++ Tests/PositionTests/PositionTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Position
+
+final class PositionTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Position().text, "Hello, World!")
+    }
+}
